159bd7d67a362dccd8a2e4518360ac38c9956e5e
chokyungjin0504 <sarahkj45@gmail.com>
2025-03-03 12:55:05 +0900

    second

--- walkthrough/webapp/controller/App.controller.js
+++ walkthrough/webapp/controller/App.controller.js
@@ -1,12 +1,24 @@
 sap.ui.define([
    "sap/ui/core/mvc/Controller",
-   "sap/m/MessageToast"
-], (Controller, MessageToast) => {
+   "sap/m/MessageToast",
+   "sap/ui/model/json/JSONModel"
+], (Controller, MessageToast, JSONModel) => {
    // define에서 controller, messagetoast 순서로 받았으니
    // 인자도 순서대로 받는다
    "use strict";
 
    return Controller.extend("ui5.walkthrough.controller.App", {
+      onInit() {
+         // set data model on view
+         const oData = {
+            recipient : {
+               name : "World"
+            }
+         };
+         const oModel = new JSONModel(oData);
+         this.getView().setModel(oModel);
+      },
+      
       onShowHello() {
          MessageToast.show("Hello World");
          //모듈 정의를 이미했으니 그 이름을 통해서 함수를 호출한다
