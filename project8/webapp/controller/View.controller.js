4de48270cf3667a4f35602b2a787563bdee379d6
chokyungjin0504 <sarahkj45@gmail.com>
2025-03-12 17:33:45 +0900

    0312

---
+++ project8/webapp/controller/View.controller.js
@@ -0,0 +1,36 @@
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", "sap/ui/model/json/JSONModel"],
+  (Controller, JSONModel) => {
+    "use strict";
+
+    return Controller.extend("project8.controller.View", {
+      onInit() {
+        var oData = {
+          data: [
+            { month: "2023-01", sales: 12000 },
+            { month: "2023-02", sales: 14000 },
+            { month: "2023-03", sales: 15000 },
+            { month: "2023-04", sales: 13000 },
+            { month: "2023-05", sales: 16000 },
+            { month: "2023-06", sales: 17000 },
+          ],
+        };
+
+        var oData2 = {
+          data: [
+            { category: "A등급", value: 50 },
+            { category: "B등급", value: 65 },
+            { category: "C등급", value: 75 },
+            { category: "D등급", value: 40 },
+          ],
+        };
+
+        var oModel = new JSONModel(oData);
+        this.getView().setModel(oModel, "chart");
+
+        var oModel2 = new JSONModel(oData2);
+        this.getView().setModel(oModel2, "chart2");
+      },
+    });
+  }
+);
---
+++ project8/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"project8/test/unit/controller/View.controller"
+], function () {
+	"use strict";
+});
